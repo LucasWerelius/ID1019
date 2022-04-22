47a1b8c1120615879feeb852600170f8c6f188d2
Lucas Werelius <97474020+LucasWerelius@users.noreply.github.com>
2022-04-22 16:26:56 +0200

    Update prim.ex

--- primtal/prim.ex
+++ primtal/prim.ex
@@ -1,11 +1,38 @@
 defmodule Prim do
   def tal(n) do
-    list = Enum.to_list(2..n)
-    IO.inspect(forstalosningen(list))
+    IO.write("Forsta: ")
+    #IO.inspect(forstalosningen(Enum.to_list(2..n)))
+    IO.inspect(:timer.tc(fn -> forstalosningen(n) end, []))
+    IO.write("Andra:  ")
+    #IO.inspect(andralosningen(Enum.to_list(2..n)))
+    IO.inspect(:timer.tc(fn -> andralosningen(n) end, []))
+    IO.write("Tredje: ")
+    #IO.inspect(tredjelosningen(Enum.to_list(2..n)))
+    IO.inspect(:timer.tc(fn -> tredjelosningen(n) end, []))
+    :ok
   end
 
-  def forstalosningen([h|t]) do
-    forstalosningen(t, [h])
+  def forstalosningen(p) do
+    forstalosningen(Enum.to_list(2..p), [])
+  end
+
+  def tabortomdelbar([], _) do [] end
+  def tabortomdelbar([h|t], x) do
+    case rem(h, x) == 0 do
+      false ->
+        [h|tabortomdelbar(t, x)]
+      true ->
+        tabortomdelbar(t, x)
+    end
+  end
+
+  def forstalosningen([], primtal) do primtal end
+  def forstalosningen([h|t], primtal) do
+    forstalosningen(tabortomdelbar(t, h), primtal ++ [h])
+  end
+
+  def andralosningen(p) do
+    andralosningen(Enum.to_list(2..p), [])
   end
   def kolladelbarhet(_, []) do true end
   def kolladelbarhet(nr, [h|t]) do
@@ -18,13 +45,35 @@ defmodule Prim do
 
   end
 
-  def forstalosningen([], tal) do tal end
-  def forstalosningen([h|t], tal) do
+  def andralosningen([], primtal) do primtal end
+  def andralosningen([h|t], primtal) do
+    case kolladelbarhet(h, primtal) do
+      true ->
+        andralosningen(t, primtal ++ [h])
+      false ->
+        andralosningen(t, primtal)
+    end
+  end
+
+
+  def tredjelosningen(p) do
+    tredjelosningen(Enum.to_list(2..p), [])
+  end
+
+  def tredjelosningen([], tal) do reverse(tal, []) end
+  def tredjelosningen([h|t], tal) do
     case kolladelbarhet(h, tal) do
       true ->
-        forstalosningen(t, [h|tal])
+        tredjelosningen(t, [h|tal])
       false ->
-        forstalosningen(t, tal)
+        tredjelosningen(t, tal)
     end
+  end
+
+  def reverse([], ny) do
+    ny
+  end
+  def reverse([h|t], ny) do
+    reverse(t, [h|ny])
   end
 end
